0f74c5c1bca43c1a7d25b10acce8ce6c5c4395c6
jenish <jenishptrt@gmail.com>
2019-08-26 17:19:09 +0530

    Added Elastic Search Ping API and NodeJS Hello word API

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+const esClient = require('../services/esService');
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.json({ message: "Hello word" });
+});
+
+router.get('/ping', async (req, res, next) => {
+  esClient.ping({
+    // ping usually has a 3000ms timeout
+    requestTimeout: 1000
+  }, async (error, data) => {
+    if (error) {
+      res.json({ message: 'elasticsearch cluster is down!' });
+    } else {
+      console.log(data)
+    }
+  });
+  const health = await esClient.cluster.health({});
+  res.json({ message: 'All is well', status: health });
+})
+module.exports = router;
